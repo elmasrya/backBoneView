6f45c9d3fbc734890de30e8414d755ec9c9d7f15
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-03 10:22:56 -0500

    n

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('The Iron Yard Rocks');
+var server= "http://tiy-atl-fe-server.herokuapp.com/collections/pharoah";
+//
+// //Student is my model
+
+
+
+var Feeling = Backbone.Model.extend({
+
+  defaults: {
+    title:'',
+    cause: '',
+    type: ''
+  },
+  //set it to the id of the api
+  idAttribute: '_id',
+
+  //make sure initialize is spelled correctly
+  initialize: function () {
+    console.log('I have been created');
+
+  }
+
+  });
+
+  var Feelings = Backbone.Collection.extend({
+      model:Feeling,
+      url: server
+      });
